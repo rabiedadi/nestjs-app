be2d1b7fdf2f628f6d075dca735d2e78ab8e8e19
rabiedadi <rabiedadi@gmail.com>
2020-11-06 22:32:41 +0100

    _

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  global.console.clear();
   const app = await NestFactory.create(AppModule);
   // const configService = app.get(ConfigService);
   app.setGlobalPrefix('api');
